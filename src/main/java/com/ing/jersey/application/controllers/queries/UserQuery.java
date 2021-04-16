2377c802d1a49692c7379331787f8ba0b9971da3
DESKTOP-JABHKMA\anthony <antonio4793@hotmail.com>
2021-04-16 08:10:53 -0500

    first commit, two jersey json over http services

---
+++ src/main/java/com/ing/jersey/application/controllers/queries/UserQuery.java
@@ -0,0 +1,34 @@
+package com.ing.jersey.application.controllers.queries;
+
+import com.ing.jersey.domain.model.entities.User;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.ArrayList;
+import java.util.List;
+
+@Path("/users")
+public class UserQuery {
+
+    private static final List<User> listaUsuarios = new ArrayList<User>() {
+        {
+            add(new User("Rosa", "Marfil"));
+            add(new User("Pepito", "Grillo"));
+            add(new User("Manuela", "Lago"));
+        }
+    };
+
+    /**
+     * URL: http://localhost:8080/API_REST_WS-RS/api/users
+     * @return Response list Users
+     */
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getUsers() {
+        return Response.ok( listaUsuarios ).build();
+    }
+
+}
