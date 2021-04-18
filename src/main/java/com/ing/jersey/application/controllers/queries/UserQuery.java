3d3de656d3e2df7321ec8a3fd158d8ea44cf812f
DESKTOP-JABHKMA\anthony <antonio4793@hotmail.com>
2021-04-18 15:56:01 -0500

    - added basic jersey GET and POST services

--- src/main/java/com/ing/jersey/application/controllers/queries/UserQuery.java
+++ src/main/java/com/ing/jersey/application/controllers/queries/UserQuery.java
@@ -2,9 +2,7 @@ package com.ing.jersey.application.controllers.queries;
 
 import com.ing.jersey.domain.model.entities.User;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.ArrayList;
@@ -28,6 +26,14 @@ public class UserQuery {
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public Response getUsers() {
+        return Response.ok( listaUsuarios ).build();
+    }
+
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response getUsers( User user ) {
+        listaUsuarios.add( user );
         return Response.ok( listaUsuarios ).build();
     }
 
---
+++ src/main/java/com/ing/jersey/domain/model/entities/Product.java
@@ -0,0 +1,12 @@
+package com.ing.jersey.domain.model.entities;
+
+public class Product {
+
+    public final String name;
+    public final float weight;
+
+    public Product( String name, float weight ) {
+        this.name = name;
+        this.weight = weight;
+    }
+}
